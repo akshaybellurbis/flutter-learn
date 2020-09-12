8a69fcba0bb33cd2405c35cf0a4a3b0e25ea5617
akshaybellurbis <akshay.kumar@bellurbis.com>
2020-09-12 20:37:08 +0530

    add new page in project

---
+++ first_project/lib/drink_detail.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DrinkDetail extends StatelessWidget {
+  final drink;
+
+  const DrinkDetail({Key key, @required this.drink}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: Text(drink["strDrink"]),
+        elevation: 0.0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.pink,
+          Colors.yellow,
+        ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: drink["idDrink"],
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: NetworkImage(drink["strDrinkThumb"]),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "ID: ${drink["idDrink"]}",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "${drink["strDrink"]}",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- first_project/lib/homepage.dart
+++ first_project/lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:first_project/drink_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -40,8 +41,11 @@ class _HomePageState extends State<HomePage> {
                 itemBuilder: (context, index) {
                   var drink = drinks[index];
                   return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(drink["strDrinkThumb"]),
+                    leading: Hero(
+                      tag: drink["idDrink"],
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(drink["strDrinkThumb"]),
+                      ),
                     ),
                     title: Text(
                       "${drink["strDrink"]}",
@@ -56,6 +60,13 @@ class _HomePageState extends State<HomePage> {
                         color: Colors.white,
                       ),
                     ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DrinkDetail(drink: drink),
+                          ));
+                    },
                   );
                 },
               )
@@ -64,48 +75,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-// class indiawidget extends StatelessWidget {
-//   const indiawidget({
-//     Key key,
-//     @required this.myText,
-//   }) : super(key: key);
-
-//   final String myText;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: <Widget>[
-//         Image.network(
-//           "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png",
-//           fit: BoxFit.contain,
-//           height: 200.0,
-//           width: 200.0,
-//         ),
-//         Row(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               myText,
-//               textScaleFactor: 2.0,
-//             ),
-//             SizedBox(
-//               width: 50.0,
-//             ),
-//             FlatButton(
-//               color: Colors.white,
-//               onPressed: () {
-//                 // myText = "Hindustani";
-//                 // print(myText);
-//                 // setState(() {});
-//               },
-//               child: Text("press me!"),
-//             )
-//           ],
-//         )
-//       ],
-//     );
-//   }
-// }
