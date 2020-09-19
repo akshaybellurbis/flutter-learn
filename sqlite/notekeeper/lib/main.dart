ce123661935662265ae857efc81b93a17f9f0fb8
akshaybellurbis <akshay.kumar@bellurbis.com>
2020-09-19 17:44:41 +0530

    add to do list

---
+++ sqlite/notekeeper/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/screens/note_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: NoteList(),
+    );
+  }
+}
---
+++ sqlite/notekeeper/lib/screens/note_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Fab clicked');
+        },
+        tooltip: 'Add Note',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getNoteListView() {
+    TextStyle titleStyle = theme.of(context).textTheme.subhead;
+    return ListView.builder(
+      itemCount: count,
+      itembuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Icon(Icon.keyboard_arrow_right),
+            ),
+            title: Text(
+              'Dummy Title',
+              style: titleStyle,
+            ),
+            subtitle: Text('Dummy Date'),
+            trailing: Icon(
+              Icon.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+            },
+          ),
+        );
+      },
+    );
+  }
+}
